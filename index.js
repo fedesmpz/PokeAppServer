e3754f120f74c7f75efc98ce4074d55df2f62238
unknown <fede.mpz@gmail.com>
2023-05-31 18:12:06 -0300

    limit 1200

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const { conn } = require('./src/db.js');
 const { PORT } = process.env;
 
 // Syncing all the models at once.
-conn.sync({ force: false }).then(() => {
+conn.sync({ force: true }).then(() => {
   console.log('DB conectada')
   server.listen(PORT, () => {
     console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
